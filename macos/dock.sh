13e4782602fa11f79d473e58f7dae30241ae7c59
Pierre-Louis Gottfrois <pierrelouis.gottfrois@gmail.com>
2022-04-15 17:58:34 +0200

    Update dock.sh

--- macos/dock.sh
+++ macos/dock.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
 dockutil --no-restart --remove all
-dockutil --no-restart --add "/Applications/Safari.app"
-dockutil --no-restart --add "/Applications/Google Chrome.app"
+dockutil --no-restart --add "/Applications/Brave.app"
 dockutil --no-restart --add "/Applications/Calendar.app"
 dockutil --no-restart --add "/Applications/App Store.app"
 dockutil --no-restart --add "/Applications/System Preferences.app"
 dockutil --no-restart --add "/Applications/iTerm.app"
-dockutil --no-restart --add "/Applications/Atom.app"
 dockutil --no-restart --add "/Applications" --view grid --display stack
 dockutil --no-restart --add "~/Downloads" --view fan --display folder
 
