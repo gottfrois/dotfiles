432506ac395123c23d193f70b07920656227ee98
Pierre-Louis Gottfrois <pierrelouis.gottfrois@gmail.com>
2023-02-21 14:18:06 +0100

    Fixes dock items

--- macos/dock.sh
+++ macos/dock.sh
@@ -3,9 +3,9 @@
 dockutil --no-restart --remove all
 dockutil --no-restart --add "/Applications/Brave Browser.app"
 dockutil --no-restart --add "/Applications/Visual Studio Code.app"
-dockutil --no-restart --add "/Applications/Calendar.app"
-dockutil --no-restart --add "/Applications/App Store.app"
-dockutil --no-restart --add "/Applications/System Settings.app"
+dockutil --no-restart --add "/System/Applications/Calendar.app"
+dockutil --no-restart --add "/System/Applications/App Store.app"
+dockutil --no-restart --add "/System/Applications/System Settings.app"
 dockutil --no-restart --add "/Applications/iTerm.app"
 dockutil --no-restart --add "/Applications" --view grid --display stack
 dockutil --no-restart --add "~/Downloads" --view fan --display folder
